48d0c4eeb31ab27007d227da38c07532b5a843b2
Sean Leishman <leishmansean@gmail.com>
2024-08-22 19:03:49 +0100

    Add bash script

--- data/switchboard/extract_swb.bash
+++ data/switchboard/extract_swb.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+curl https://isip.piconepress.com/projects/switchboard/releases/switchboard_word_alignments.tar.gz -o switchboard.tar.gz;
 tar -xvzf switchboard.tar.gz;
+mv swb_ms98_transcriptions switchboard/switchboard1/transcriptions;
